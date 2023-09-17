89a5507b0c7a1f96b583ac44e041f1549a47cc8f
Cristian Prochnow <cristianprochnow@gmail.com>
2023-09-17 20:43:58 -0300

    feat: add custom fonts and SafeAreaView

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useCallback} from 'react';
+import {Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {StatusBar} from 'expo-status-bar';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  let [isFontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  const onLayoutRootBuilt = useCallback(async () => {
+    if (isFontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isFontsLoaded]);
+
+  if (!isFontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView onLayout={onLayoutRootBuilt}>
+      <StatusBar style='dark'/>
+      <View>
+        <Text style={{
+          fontFamily: 'Poppins_700Bold'
+        }}>Testing the Expo App!</Text>
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
