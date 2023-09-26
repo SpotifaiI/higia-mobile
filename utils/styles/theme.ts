6b0b0f02d79e8bd6e675727095cf7603ad6f1f34
Cristian Prochnow <cristianprochnow@gmail.com>
2023-09-25 23:24:31 -0300

    feat: add and update styles

---
+++ utils/styles/theme.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  main1: '#37FC6E',
+  main2: '#5A81FA',
+  darker: '#333333',
+  dark: '#787D82',
+  light: '#E6F3F6',
+  lighter: '#F5F5F5',
+  white: '#FBFBFB',
+  black: '#101010',
+  danger: '#DC3545'
+};
