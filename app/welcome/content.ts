ceede71941f521f01443eb074811e40d6fd0681a
Cristian Prochnow <cristianprochnow@gmail.com>
2023-09-30 21:17:34 -0300

    fix: fix dinamic data from welcome screen

---
+++ app/welcome/content.ts
@@ -0,0 +1,31 @@
+import { welcome } from "../../global/assets/images";
+
+export const welcomeContent = [
+  {
+    key: 0,
+    title: 'Bem-vindo ao Higia!',
+    image: welcome[0].hero,
+    step_title: 'Simplificando a Limpeza Urbana para Você.',
+    subtitle: 'Comece a gerenciar suas tarefas de limpeza de forma eficiente e veja a sua cidade brilhar.',
+    button_label: 'Explorar recursos',
+    step_image: welcome[0].step
+  },
+  {
+    key: 1,
+    title: 'Explore todos os recursos!',
+    image: welcome[1].hero,
+    step_title: 'Facilitando seu Dia a Dia de Trabalho.',
+    subtitle: 'Gerencie suas tarefas do dia a dia, visualize no mapa todos os afazeres e acompanhe estatísticas sobre sua rotina de trabalho.<br><br>Tudo isso em um só lugar!',
+    button_label: 'Avançar para começar',
+    step_image: welcome[1].step
+  },
+  {
+    key: 2,
+    title: 'É hora de agir!',
+    image: welcome[2].hero,
+    step_title: 'Junte-se a nós na missão de tornar a cidade mais limpa e sustentável.',
+    subtitle: 'Comece a explorar o Higia e faça a diferença agora!',
+    button_label: 'Começar agora!',
+    step_image: welcome[2].step
+  }
+];
---
+++ global/assets/images.ts
@@ -0,0 +1,14 @@
+export const welcome = [
+  {
+    hero: require('../../assets/images/welcome/0-welcome.png'),
+    step: require('../../assets/images/welcome/0-step.png')
+  },
+  {
+    hero: require('../../assets/images/welcome/1-welcome.png'),
+    step: require('../../assets/images/welcome/1-step.png')
+  },
+  {
+    hero: require('../../assets/images/welcome/2-welcome.png'),
+    step: require('../../assets/images/welcome/2-step.png')
+  },
+];
