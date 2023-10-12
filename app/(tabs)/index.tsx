0fa487052cbe397486d47faf32da6169e48b36fe
gustapine <gustavohd34@gmail.com>
2023-10-12 14:25:15 -0300

    feat: beginning home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,34 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import { useEffect, useMemo, useState } from "react";
+import {
+  Container,
+  ContentContainer,
+  Description,
+  Footer,
+  Phrase,
+  TextWrapper,
+  Title,
+  StyledProgressBar
+} from './styles';
 
-import { Container } from "./styles";
 
 export default function Home() {
   return (
     <Container>
-      <Text>
-        Você está na tela inicial das tabs
-      </Text>
+      <Title>Bom dia, João!</Title>
+      <View>
+        <StyledProgressBar progress={0.5} width={200} height={20} />
+      </View>
+
+      <ContentContainer>
+        <TextWrapper>
+          <Phrase>Hello, World!</Phrase>
+          <Description>Show de boletas</Description>
+        </TextWrapper>
+      </ContentContainer>
+
+      <Footer>
+      </Footer>
     </Container>
   );
 }
--- app/(tabs)/styles.ts
+++ app/(tabs)/styles.ts
@@ -1,7 +1,60 @@
-import styled from 'styled-components/native';
+import MaskedView from "@react-native-masked-view/masked-view";
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+import ProgressBar from 'react-native-progress/Bar';
+
+import { colors } from "../../global/styles/theme";
 
 export const Container = styled.View`
-  width: 60%;
-  height: 60%;
-  background-color: #000;
+  width: 100%;
+  height: 100%;
+  padding: ${RFValue(16)}px 0;
+  justify-content: space-between;
+`;
+
+export const Title = styled.Text`
+  font-size: ${RFValue(28)}px;
+  line-height: ${RFValue(48)}px;
+  font-family: 'Poppins_700Bold';
+  color: ${colors.darker};
+  text-align: left;
+  margin-left: ${RFValue(25)}px;
+`;
+
+export const ContentContainer = styled.View`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Phrase = styled.Text`
+  font-size: ${RFValue(24)}px;
+  line-height: ${RFValue(32)}px;
+  font-family: 'Poppins_700Bold';
+  color: ${colors.darker};
+  width: 100%;
+  text-align: center;
+`;
+
+export const Description = styled.Text`
+  font-size: ${RFValue(16)}px;
+  line-height: ${RFValue(24)}px;
+  font-family: 'Poppins_500Medium';
+  color: ${colors.dark};
+  width: 100%;
+  text-align: center;
+`;
+
+export const TextWrapper = styled.View`
+  width: 90%;
+  gap: ${RFValue(16)}px;
+`;
+
+export const Footer = styled.View`
+  gap: ${RFValue(24)}px;
+`;
+
+
+export const StyledProgressBar = styled(ProgressBar)`
+  color: ${colors.darker};
 `;
