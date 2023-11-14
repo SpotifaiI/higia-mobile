2094434598bee3bb7f1e4f8e2ad288c6ae7ced89
gustapine <gustavohd34@gmail.com>
2023-11-14 20:42:51 -0300

    feat: creating map settings screen

--- app/(tabs)/settings/SettingsMap/index.tsx
+++ app/(tabs)/settings/SettingsMap/index.tsx
@@ -1,10 +1,61 @@
-import { View, Text } from "react-native";
+import { View, Text, Switch, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { colors } from "../../../../global/styles/theme";
+import {
+  TaksMap,
+  Container,
+  TasksText,
+  SizeIcons,
+  MapButtonDescription,
+  AntDesignWrapper,
+  MapButtonText,
+} from "./styles";
+import { useNavigation } from "@react-navigation/native";
+import { AntDesign } from "@expo/vector-icons";
 
 export const SettingsMap: React.FC = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>Configurações de Mapa</Text>
-      {/* Adicione componentes e lógica específicos para Aparência aqui */}
-    </View>
+    <Container>
+      <TouchableOpacity
+        style={SizeIcons}
+        onPress={()=>{}}
+      >
+        <MapButtonText>
+          <MapButtonDescription>Aparência</MapButtonDescription>
+          <AntDesignWrapper>
+            <AntDesign name="arrowright" size={18} color="black" />
+          </AntDesignWrapper>
+        </MapButtonText>
+      </TouchableOpacity>
+      <TaksMap>
+        <TasksText>Mostrar tarefas concluídas</TasksText>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <View
+            style={{
+              backgroundColor: isEnabled ? colors.light : colors.light,
+              borderRadius: 25,
+              paddingRight: 13,
+              paddingLeft: 10,
+              paddingBottom: 50,
+              paddingTop: 0,
+              height: 40,
+            }}
+          >
+            <Switch
+              trackColor={{ false: "transparent", true: "transparent" }}
+              thumbColor={isEnabled ? colors.main1 : colors.main1Light}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={toggleSwitch}
+              value={isEnabled}
+              style={{ transform: [{ scale: 1.3 }] }}
+            />
+          </View>
+        </View>
+      </TaksMap>
+    </Container>
   );
 };
--- app/(tabs)/settings/SettingsMap/styles.ts
+++ app/(tabs)/settings/SettingsMap/styles.ts
@@ -1,21 +1,55 @@
-// styles.ts
-import { StyleSheet } from "react-native";
+import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+import { colors } from "../../../../global/styles/theme";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  scrollView: {
-    padding: 16,
-  },
-  section: {
-    marginBottom: 20,
-  },
-  sectionTitle: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginBottom: 8,
-  },
-});
 
-export default styles;
+
+export const Container = styled.View`
+  padding: ${RFValue(16)}px;
+  display: flex;
+`;
+
+export const TaksMap = styled.View`
+  background-color: ${colors.lighter};
+  border-radius: 16px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  display: flex;
+  margin-top: 20px;
+  padding: 7px 15px 7px 25px;
+  width: 100%;
+`;
+
+export const TasksText = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: Poppins_400Regular;
+  color: ${colors.black};
+`;
+
+export const SizeIcons = {
+  backgroundColor: colors.lighter,
+  padding: 16,
+  borderRadius: 16,
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  display: 'flex',
+};
+
+export const MapButtonText = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px 15px 0px 11px;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+export const MapButtonDescription = styled.Text`
+  font-family: "Poppins_700Bold";
+  font-size: ${RFValue(15)}px;
+  display: flex;
+`;
+
+export const AntDesignWrapper = styled.View``;
